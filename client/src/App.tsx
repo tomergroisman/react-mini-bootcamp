d7d76f48fa6a6a318896719faa0bf6b95afa1c5d
Tomer Groisman <tomergroisman@gmail.com>
2021-04-12 17:09:14 +0300

    Bootcamp setup

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * TODO: (Client basics)
+ * - DOM and rendering
+ * - React basics (create a counter button component):
+ *      JSX (TSX)
+ *      States
+ *      Props
+ *      Rendering
+ *      Callbacks
+ *      React events
+ * - React class component and functional component
+ * 
+ * TODO: (Client-Server app):
+ * - Creat the app structure:
+ *      List of Pokemons (names only)
+ *      Paging bar
+ *      Search bar
+ * - Create an api object to help us talk to the server
+ */
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
---
+++ server/server.ts
@@ -0,0 +1,16 @@
+const express = require('express');
+
+/**
+ * TODO: (Server Basics)
+ * - Set a simple server with '/' route, listening on port 3000
+ * - Cover the basics of REST API:
+ *      request and response
+ *      query
+ *      body
+ *      dynamic route
+ * 
+ * TODO: (Client-Server App)
+ * - Add a GET pokemons route
+ * - Add a page query to the route
+ * - Add search query to the route
+ */
